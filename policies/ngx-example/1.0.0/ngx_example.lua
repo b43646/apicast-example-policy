5ae19d8b830a179944c60258886f06292b6bb2da
loren <loren@redhat.com>
2019-05-05 12:50:29 +0000

    s

--- policies/ngx-example/1.0.0/ngx_example.lua
+++ policies/ngx-example/1.0.0/ngx_example.lua
@@ -6,16 +6,20 @@ function _M.new(configuration)
   local self = new(configuration)
 
   ngx.req.read_body()
---  local args, err = ngx.req.get_post_args()
---  local ret = ngx.re.match(val, '.*%-%-.*')
---  if ret then
---      ngx.say("invalid username or password")
---  elseif key == "password" then
---      local ret2 = ngx.re.match(val, ".*%-%-.*")
---      if ret2 then
---          ngx.say("invalid username or password")
---      end
---  end
+  local args, err = ngx.req.get_post_args()
+  for key, val in pairs(args) do
+  	if key == "password" then
+  		local ret = ngx.re.match(val, '.*%-%-.*')
+  		if ret then
+      			ngx.say("invalid username or password")
+                end
+  	elseif key == "password" then
+      		local ret2 = ngx.re.match(val, ".*%-%-.*")
+      		if ret2 then
+          		ngx.say("invalid username or password")
+      		end
+  	end
+  end
 
   return self
 end
