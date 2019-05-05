fa52dac0aefda830440b2259013b17cc71c10710
loren <loren@redhat.com>
2019-05-05 10:17:10 +0000

    add injection

--- policies/ngx-example/1.0.0/ngx_example.lua
+++ policies/ngx-example/1.0.0/ngx_example.lua
@@ -15,8 +15,23 @@ function _M.new(configuration)
 
   for _, header in ipairs(set_header) do
     insert(ops, function()
-      ngx.log(ngx.NOTICE, 'setting header: ', header.name, ' to: ', header.value)
-      ngx.req.set_header(header.name, header.value)
+      -- ngx.log(ngx.NOTICE, 'setting header: ', header.name, ' to: ', header.value)
+      -- ngx.req.set_header(header.name, header.value)
+      ngx.req.read_body()
+      local args, err = ngx.req.get_post_args()
+      for key, val in pairs(args) do
+        if key == "username" then
+          local ret = ngx.re.match(val, '*--*')
+          if ret then
+            ngx.say("invalid username or password")
+          end
+        elseif key == "password" then
+          local ret2 = ngx.re.match(val, '*--*')
+          if ret2 then
+            ngx.say("invalid username or password")
+          end
+        end
+      end
     end)
   end
 
