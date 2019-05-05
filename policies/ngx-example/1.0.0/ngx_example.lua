c7a41e28a9651f91e0e20163cc293d997a061c49
loren <loren@redhat.com>
2019-05-05 12:56:38 +0000

    s

--- policies/ngx-example/1.0.0/ngx_example.lua
+++ policies/ngx-example/1.0.0/ngx_example.lua
@@ -2,6 +2,8 @@ local _M = require('apicast.policy').new('Nginx Example', '1.0.0')
 
 local new = _M.new
 
+content='Hello World'
+
 function _M.new(configuration)
   local self = new(configuration)
 
@@ -12,16 +14,23 @@ function _M.new(configuration)
   		local ret = ngx.re.match(val, '.*%-%-.*')
   		if ret then
       			ngx.say("invalid username or password")
+                        content="invalid username or password"
                 end
   	elseif key == "password" then
       		local ret2 = ngx.re.match(val, ".*%-%-.*")
       		if ret2 then
           		ngx.say("invalid username or password")
+                        content="invalid username or password"
       		end
   	end
   end
 
   return self
 end
+
+function _M:content()
+  return content
+end
+
 
 return _M
