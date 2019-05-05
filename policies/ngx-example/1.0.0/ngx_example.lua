005350d3529c3fb6a8c6f62ed79c32a7ad0002ac
loren <loren@redhat.com>
2019-05-05 12:34:21 +0000

    s

--- policies/ngx-example/1.0.0/ngx_example.lua
+++ policies/ngx-example/1.0.0/ngx_example.lua
@@ -5,35 +5,19 @@ local new = _M.new
 function _M.new(configuration)
   local self = new(configuration)
 
-  ngx.req.read_body()
-  local args, err = ngx.req.get_post_args()
-  local ret = ngx.re.match(val, '.*%-%-.*')
-  if ret then
-      ngx.say("invalid username or password")
-  elseif key == "password" then
-      local ret2 = ngx.re.match(val, ".*%-%-.*")
-      if ret2 then
-          ngx.say("invalid username or password")
-      end
-  end
+--  ngx.req.read_body()
+--  local args, err = ngx.req.get_post_args()
+--  local ret = ngx.re.match(val, '.*%-%-.*')
+--  if ret then
+--      ngx.say("invalid username or password")
+--  elseif key == "password" then
+--      local ret2 = ngx.re.match(val, ".*%-%-.*")
+--      if ret2 then
+--          ngx.say("invalid username or password")
+--      end
+--  end
 
   return self
 end
-
-
-function access()
-      ngx.req.read_body()
-      local args, err = ngx.req.get_post_args()
-      local ret = ngx.re.match(val, '*\-\-*')
-      if ret then
-          ngx.say("invalid username or password")
-      elseif key == "password" then
-          local ret2 = ngx.re.match(val, '*\-\-*')
-          if ret2 then
-              ngx.say("invalid username or password")
-          end
-      end
-end
-
 
 return _M
