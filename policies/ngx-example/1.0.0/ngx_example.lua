1bd3379230c0d25296aab43a41ea2b2df70b8b5b
loren <loren@redhat.com>
2019-05-05 12:13:52 +0000

    s

--- policies/ngx-example/1.0.0/ngx_example.lua
+++ policies/ngx-example/1.0.0/ngx_example.lua
@@ -2,26 +2,20 @@ local _M = require('apicast.policy').new('Nginx Example', '1.0.0')
 
 local new = _M.new
 
-local ipairs = ipairs
-local insert = table.insert
-
 function _M.new(configuration)
   local self = new()
 
-  local ops = {}
-
-  local config = configuration or {}
-  local set_header = config.set_header or {}
-
-  -- for _, header in ipairs(set_header) do
-  --  insert(ops, function()
-  --    ngx.log(ngx.NOTICE, 'setting header: ', header.name, ' to: ', header.value)
-  --    ngx.req.set_header(header.name, header.value)
-  --  end)
-  -- insert(ops,access())
-  -- end
-
-  self.ops = ops
+  ngx.req.read_body()
+  local args, err = ngx.req.get_post_args()
+  local ret = ngx.re.match(val, '*\-\-*')
+  if ret then
+      ngx.say("invalid username or password")
+  elseif key == "password" then
+      local ret2 = ngx.re.match(val, ".*%-%-.*")
+       if ret2 then
+           ngx.say("invalid username or password")
+       end
+  end
 
   return self
 end
